b6b28a4c6a1ccad827f2c0fb80db17a88168ba99
passionofvc <yamada.passion@gmail.com>
2019-10-11 23:28:38 +0900

    format output

--- btc/check_btc_balance.sh
+++ btc/check_btc_balance.sh
@@ -5,18 +5,18 @@ priv="$@"
 addr=$( bx ec-to-public "${priv}" | bx ec-to-address )
 #balance=$( curl -s https://insight.bitpay.com/api/addrs/${addr}/utxo | jq -r ".[] | [.address,.txid,.vout,.      scriptPubKey,.amount] | @tsv" )
 balance=$( curl -s https://insight.bitpay.com/api/addr/${addr}/balance )
-echo "btc:" ${addr} ${priv} ${balance}
+echo "btc:" ${addr} "           "${priv} ${balance}
 
 #uncompressed
 addr=$( bx ec-to-public -u ${priv} | bx ec-to-address )
 balance=$( curl -s https://insight.bitpay.com/api/addr/${addr}/balance )
-echo "btc:" ${addr} ${priv} ${balance}
+echo "btc:" ${addr} "           "${priv} ${balance}
 
 
 #btc[p2sh-p2wpkh]-------------------------------------------------------
 addr=$( node gen_p2sh_p2wpkh_addr.js "${priv}")
 balance=$( curl -s https://insight.bitpay.com/api/addr/${addr}/balance )
-echo "btc:" ${addr} ${priv} ${balance}
+echo "btc:" ${addr} "           "${priv} ${balance}
 
 
 #btc[p2wpkh]-------------------------------------------------------
@@ -24,5 +24,5 @@ addr=$( node gen_p2wpkh_addr.js "${priv}")
 balance=$( curl -s https://blockstream.info/api/address/${addr} | jq .chain_stats )
 fund=$(echo ${balance}  | jq .funded_txo_sum )
 spent=$(echo ${balance} | jq .spent_txo_sum  )
-echo "btc:" ${addr} ${priv} $(( $fund - $spent  ))
+echo "btc:" ${addr} "   "${priv} $(( $fund - $spent  ))
 
--- eth/check_eth_balance.sh
+++ eth/check_eth_balance.sh
@@ -2,5 +2,5 @@ priv="$@"
 addr=$(node eth_priv_2_addr.js ${priv})
 balance=$( curl -s "https://api.etherscan.io/api?module=account&action=balancemulti&address=${addr}&tag=latest&apikey=YourApiKeyToken" | jq -r .result[].balance )
 
-echo "eth: "${addr} ${priv} ${balance}
+echo "eth: "${addr} "  " ${priv} ${balance}
 
