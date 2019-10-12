60496816f374fd130e4f2b06626ac6751afb6762
passionofvc <yamada.passion@gmail.com>
2019-10-12 11:17:48 +0900

    add xrp check balance

--- check_coin_balance.sh
+++ check_coin_balance.sh
@@ -1,4 +1,5 @@
 ( cd btc && bash check_btc_balance.sh "$@"  )
 ( cd eth && bash check_eth_balance.sh "$@" )
 ( cd ltc && bash check_ltc_balance.sh "$@" )
+( cd xrp && bash check_xrp_balance.sh "$@" )
 
