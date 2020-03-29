f7e01e82a3eee4b9037cf19f763b147aa38b5441
passionofvc <yamada.passion@gmail.com>
2020-03-29 15:06:54 +0900

    add brain check

--- check_coin_balance_b.sh
+++ check_coin_balance_b.sh
@@ -4,14 +4,14 @@ priv=$( echo -n "$@" | shasum -a 256 | awk '{print $1}' )
 addr=$( bx ec-to-public ${priv} | bx ec-to-address )
 #balance=$( curl -s https://insight.bitpay.com/api/addrs/${addr}/utxo | jq -r ".[] | [.address,.txid,.vout,.scriptPubKey,.amount] | @tsv" )
 balance=$( curl -s https://insight.bitpay.com/api/addr/${addr}/balance )
-echo ${addr} ${priv} ${balance}
+echo "btc:" ${addr} "           "${priv} ${balance}
 if [[ "${balance}" != "0" ]]; then
     echo ${addr} ${priv} ${balance}
 fi
 
 addr=$( bx ec-to-public -u ${priv} | bx ec-to-address )
 balance=$( curl -s https://insight.bitpay.com/api/addr/${addr}/balance )
-echo ${addr} ${priv} ${balance}
+echo "btc:" ${addr} "           "${priv} ${balance}
 if [[ "${balance}" != "0" ]]; then
     echo ${addr} ${priv} ${balance}
 fi
@@ -22,7 +22,7 @@ addr=$( bx ec-to-public ${priv} | bx ec-to-address )
 #balance=$( curl -s https://blockdozer.com/api/addr/${addr}/balance )
 #balance=$( curl -s https://bch.coin.space/api/addr/${addr}/balance )
 balance=$( curl -s https://api.bitcore.io/api/BCH/mainnet/address/${addr}/balance | jq .balance )
-echo ${addr} ${priv} ${balance}
+echo "bcc:" ${addr} "           "${priv} ${balance}
 if [[ "${balance}" != "0" ]]; then
     echo ${addr} ${priv} ${balance}
 fi
@@ -31,7 +31,7 @@ addr=$( bx ec-to-public -u ${priv} | bx ec-to-address )
 #balance=$( curl -s https://blockdozer.com/api/addr/${addr}/balance )
 #balance=$( curl -s https://bch.coin.space/api/addr/${addr}/balance )
 balance=$( curl -s https://api.bitcore.io/api/BCH/mainnet/address/${addr}/balance | jq .balance )
-echo ${addr} ${priv} ${balance}
+echo "bcc:" ${addr} "           "${priv} ${balance}
 if [[ "${balance}" != "0" ]]; then
     echo ${addr} ${priv} ${balance}
 fi
@@ -40,29 +40,20 @@ fi
 addr=$( bx ec-to-public ${priv} | bx ec-to-address )
 #balance=$( curl -s https://insight.bitpay.com/api/addrs/${addr}/utxo | jq -r ".[] | [.address,.txid,.vout,.scriptPubKey,.amount] | @tsv" )
 balance=$( curl -s https://bchsvexplorer.com/api/addr/${addr}/balance )
-echo ${addr} ${priv} ${balance}
+echo "bsv:" ${addr} "           "${priv} ${balance}
 if [[ "${balance}" != "0" ]]; then
     echo ${addr} ${priv} ${balance}
 fi
 
 addr=$( bx ec-to-public -u ${priv} | bx ec-to-address )
 balance=$( curl -s https://bchsvexplorer.com/api/addr/${addr}/balance )
-echo ${addr} ${priv} ${balance}
-if [[ "${balance}" != "0" ]]; then
-    echo ${addr} ${priv} ${balance}
-fi
-#eth
-priv=$( echo -n "$@" | keccak-256sum | awk '{print $1}' )
-#pubkey=$( echo -n ${priv} | bx ec-to-public)
-pubkey=$( echo -n ${priv} | bx ec-to-public -u  | sed -e 's/^04//g')
-addr=$(echo -n ${pubkey} |  keccak-256sum -x -l | tr -d ' -' | tail -c 41)
-addr='0x'"${addr}"
-#echo '0x'${addr} ${privkey}
-balance=$( curl -s "https://api.etherscan.io/api?module=account&action=balancemulti&address=${addr}&tag=latest&apikey=YourApiKeyToken" | jq -r .result[].balance )
-echo ${addr} ${priv} ${balance}
+echo "bsv:" ${addr} "           "${priv} ${balance}
 if [[ "${balance}" != "0" ]]; then
     echo ${addr} ${priv} ${balance}
 fi
 
+#eth
+( cd eth && bash check_eth_balance_b.sh "$@" )
+
 #xrp
- ( cd xrp && bash check_xrp_balance_b.sh "$@" )
+( cd xrp && bash check_xrp_balance_b.sh "$@" )
