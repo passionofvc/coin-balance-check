afc2796753b9fc0d5d3f1c96f755657afd8b7a22
passionofvc <yamada.passion@gmail.com>
2020-11-22 10:35:15 +0900

    fix insight not found

--- btc/check_btc_balance.sh
+++ btc/check_btc_balance.sh
@@ -3,20 +3,26 @@ priv="$@"
 #btc[p2pkh]-------------------------------------------------------
 #compress
 addr=$( bx ec-to-public "${priv}" | bx ec-to-address )
+balance=$( curl -s https://blockstream.info/api/address/${addr} | jq .chain_stats )
+fund=$(echo ${balance}  | jq .funded_txo_sum )
+spent=$(echo ${balance} | jq .spent_txo_sum  )
+echo "btc:" ${addr} "   "${priv} $(( $fund - $spent  ))
 #balance=$( curl -s https://insight.bitpay.com/api/addrs/${addr}/utxo | jq -r ".[] | [.address,.txid,.vout,.      scriptPubKey,.amount] | @tsv" )
-balance=$( curl -s https://insight.bitpay.com/api/addr/${addr}/balance )
-echo "btc:" ${addr} "           "${priv} ${balance}
 
 #uncompressed
 addr=$( bx ec-to-public -u ${priv} | bx ec-to-address )
-balance=$( curl -s https://insight.bitpay.com/api/addr/${addr}/balance )
-echo "btc:" ${addr} "           "${priv} ${balance}
+balance=$( curl -s https://blockstream.info/api/address/${addr} | jq .chain_stats )
+fund=$(echo ${balance}  | jq .funded_txo_sum )
+spent=$(echo ${balance} | jq .spent_txo_sum  )
+echo "btc:" ${addr} "   "${priv} $(( $fund - $spent  ))
 
 
 #btc[p2sh-p2wpkh]-------------------------------------------------------
 addr=$( node gen_p2sh_p2wpkh_addr.js "${priv}")
-balance=$( curl -s https://insight.bitpay.com/api/addr/${addr}/balance )
-echo "btc:" ${addr} "           "${priv} ${balance}
+balance=$( curl -s https://blockstream.info/api/address/${addr} | jq .chain_stats )
+fund=$(echo ${balance}  | jq .funded_txo_sum )
+spent=$(echo ${balance} | jq .spent_txo_sum  )
+echo "btc:" ${addr} "   "${priv} $(( $fund - $spent  ))
 
 
 #btc[p2wpkh]-------------------------------------------------------
