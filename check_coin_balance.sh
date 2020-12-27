970ad894ff66374c57f9d798ad71c4b24d3ddc2a
passionofvc <yamada.passion@gmail.com>
2020-12-27 12:31:00 +0900

    add 0x

--- check_coin_balance.sh
+++ check_coin_balance.sh
@@ -1,5 +1,6 @@
-( cd btc && bash check_btc_balance.sh "$@"  )
-( cd eth && bash check_eth_balance.sh "$@" )
-( cd ltc && bash check_ltc_balance.sh "$@" )
-( cd xrp && bash check_xrp_balance.sh "$@" )
+priv=$( echo "$@" | sed 's/^0x//g' )
+( cd btc && bash check_btc_balance.sh "$priv"  )
+( cd eth && bash check_eth_balance.sh "$priv" )
+( cd ltc && bash check_ltc_balance.sh "$priv" )
+( cd xrp && bash check_xrp_balance.sh "$priv" )
 
